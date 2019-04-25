e7624415b9cf50d49cdcd19e293ba1c2f21b90ef
7m7m7m <1768000453@qq.com>
2019-04-25 10:44:50 +0800

    修改、查看功能以及相应权限完成提交

--- guns-admin/src/main/webapp/static/modular/follow/follow/follow.js
+++ guns-admin/src/main/webapp/static/modular/follow/follow/follow.js
@@ -157,23 +157,86 @@ Follow.openAddFollow = function () {
     });
     this.layerIndex = index;
 };
-
 /**
- * 打开查看follow详情
+ * 修改随访表详情
  */
-Follow.openFollowDetail = function () {
+Follow.openCtephDetail = function () {
     if (this.check()) {
+        var user = 0;
+        var ajax = new $ax(Feng.ctxPath + "/mgr/currentuserid", function (data) {
+            user = data;
+        });
+        ajax.start();
+        var selected = $('#' + this.id).bootstrapTable('getSelections');
+        Follow.seItem = selected[0];
+        if (user != Follow.seItem.fillingperson) {
+            Feng.error("没有权限修改其他用户创建的数据！")
+        } else {
+            if (window.innerHeight)
+                var winHeight = window.innerHeight;         //此方式获取的是这个窗口的高度的字符串
+            else if ((document.body) && (document.body.clientHeight))
+                var winHeight = document.body.clientHeight;
+            var layheight = String(parseInt(winHeight) - 30) + 'px';    //字符串转为数字进行计算，再通过String()转为字符串
+            var index = layer.open({
+                type: 2,
+                title: '随访表详情',
+                area: ['800px', layheight], //宽高
+                fix: false, //不固定
+                maxmin: true,
+                content: Feng.ctxPath + '/follow/follow_update/' + Follow.seItem.id
+            });
+            this.layerIndex = index;
+        }
+    }
+};
+// Follow.openFollowDetail = function () {
+//     if (this.check()) {
+//         var index = layer.open({
+//             type: 2,
+//             title: 'follow详情',
+//             area: ['800px', '420px'], //宽高
+//             fix: false, //不固定
+//             maxmin: true,
+//             content: Feng.ctxPath + '/follow/follow_update/' + Follow.seItem.id
+//         });
+//         this.layerIndex = index;
+//     }
+// };
+/**
+ * 查看随访表详情
+ */
+Follow.viewCtephDetail = function () {
+    if (this.check()) {
+        if (window.innerHeight)
+            var winHeight = window.innerHeight;         //此方式获取的是这个窗口的高度的字符串
+        else if ((document.body) && (document.body.clientHeight))
+            var winHeight = document.body.clientHeight;
+        var layheight = String(parseInt(winHeight) - 30) + 'px';    //字符串转为数字进行计算，再通过String()转为字符串
         var index = layer.open({
             type: 2,
-            title: 'follow详情',
-            area: ['800px', '420px'], //宽高
+            title: '随访表详情',
+            area: ['800px', layheight], //宽高
             fix: false, //不固定
             maxmin: true,
-            content: Feng.ctxPath + '/follow/follow_update/' + Follow.seItem.id
+            content: Feng.ctxPath + '/follow/follow_view/' + Follow.seItem.id
         });
         this.layerIndex = index;
     }
 };
+// Follow.viewFollowDetail= function () {
+//     if (this.check()) {
+//         var index = layer.open({
+//             type: 2,
+//             title: 'follow详情',
+//             area: ['800px', '420px'], //宽高
+//             fix: false, //不固定
+//             maxmin: true,
+//             content: Feng.ctxPath + '/follow/follow_update/' + Follow.seItem.id
+//         });
+//         this.layerIndex = index;
+//     }
+// };
+
 /**
  * 删除follow
  */
